4adc0525b6e9a80d64f98f656eb30d93e052969f
scisneromam <scisneromam@t-online.de>
2020-09-03 14:25:13 +0200

    feat: Add scrollpane
    Scrollpane now works as expected

--- src/main/kotlin/gettinggoodguys/gui/MainView.kt
+++ src/main/kotlin/gettinggoodguys/gui/MainView.kt
@@ -9,29 +9,32 @@ class MainView : View("Getting Good Guys - by BUCKFAE") {
     val controller: MainViewController by inject()
 
     override val root = borderpane {
-        this.setMinSize(500.0, 500.0)
-
-        this.useMaxSize
+        //this.setMinSize(500.0, 500.0)
+        this.useMaxSize = true
 
         center {
-            flowpane {
+            scrollpane {
+                isFitToWidth = true
+                isFitToHeight = true
+                flowpane {
 
-                // Style of the flowpane
-                style {
-                    fontWeight = FontWeight.EXTRA_BOLD
-                    borderColor += box(
-                        top = Color.RED,
-                        right = Color.DARKGREEN,
-                        left = Color.ORANGE,
-                        bottom = Color.PURPLE
-                    )
-                }
+                    // Style of the flowpane
+                    style {
+                        fontWeight = FontWeight.EXTRA_BOLD
+                        borderColor += box(
+                            top = Color.RED,
+                            right = Color.DARKGREEN,
+                            left = Color.ORANGE,
+                            bottom = Color.PURPLE
+                        )
+                    }
 
-                // Creating and updating gameData
-                controller.initGameData()
+                    // Creating and updating gameData
+                    controller.initGameData()
 
-                for (dataGridID in 0 until controller.gamesToDisplay) {
-                    controller.drawGame(dataGridID, this)
+                    for (dataGridID in 0 until controller.gamesToDisplay) {
+                        controller.drawGame(dataGridID, this)
+                    }
                 }
             }
         }
